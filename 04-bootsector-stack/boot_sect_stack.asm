45d8fd3388928e7553def0ed2c51af2186780588
nakamura <nakamura@com>
2020-05-03 16:30:02 +0900

    stack

--- 04-bootsector-stack/boot_sect_stack.asm
+++ 04-bootsector-stack/boot_sect_stack.asm
@@ -3,12 +3,22 @@ mov ah, 0x0e ; tty mode
 mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
 mov sp, bp ; if the stack is empty then sp points to bp
 
+mov al, bl ; 0x8000 - 2
+int 0x10
+
 push 'A'
+
 push 'B'
 push 'C'
 
 ; to show how the stack grows downwards
 mov al, [0x7ffe] ; 0x8000 - 2
+int 0x10
+
+mov al, [0x7ffc] ; 0x8000 - 4
+int 0x10
+
+mov al, [0x7ffa] ; 0x8000 - 6
 int 0x10
 
 ; however, don't try to access [0x8000] now, because it won't work
