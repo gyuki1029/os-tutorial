d668cbcb9c142db256213beef9ef752b8827971c
nakamura <nakamura@com>
2020-05-07 20:34:19 +0900

    install gcc binutils

---
+++ i386-elf-gcc/lib/gcc/i386-elf/9.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-9.3.0/configure --target=i386-elf --prefix=/Users/Yuki/os-tutorial/i386-elf-gcc/ --disable-nls --disable-libssp --enable-languages=c --without-headers --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpfr=/usr/local --with-libiconv-prefix=/usr/local";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i386" }, { "arch", "i386" } };
