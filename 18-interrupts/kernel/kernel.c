8a69dfac07abeaa2d319ba0a842c74e53193fd7f
nakamura <nakamura@com>
2020-05-09 17:15:19 +0900

    set c

--- 18-interrupts/kernel/kernel.c
+++ 18-interrupts/kernel/kernel.c
@@ -8,4 +8,8 @@ void main() {
     /* Test the interrupts */
     __asm__ __volatile__("int $2");
     __asm__ __volatile__("int $3");
+
+    // int *ip = (int *)malloc(100 * sizeof(int)); // 14 Page fault (pushes an error code)
+
+    int a = 1/0; // 0 Division by zero exception
 }
